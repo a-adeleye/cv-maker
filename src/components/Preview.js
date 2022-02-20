115a5e8e7e07997a553a5d03da1f44ee24654c08
aadeleye <adeleyeadeyemib@gmail.com>
2022-02-20 10:20:07 +0100

    type one preview done

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,171 +1,15 @@
 import React from "react";
 import "../styles/TypeOne.css";
 import { useSelector, useDispatch } from "react-redux";
-import {
-  selectTemplate,
-  selectProfile,
-  selectEducation,
-  selectSkills,
-  selectExperience,
-  selectCertification,
-  selectGeneralDetails,
-} from "../resumeSlice";
-import { nanoid } from "nanoid";
+import TypeOne from "./Templates/TypeOne";
+import TypeTwo from "./Templates/TypeTwo";
+import TypeThree from "./Templates/TypeThree";
 import { Link } from "react-router-dom";
-
-// LEFT SECTION
-
-function Address() {
-  const generalDetails = useSelector(selectGeneralDetails);
-  return (
-    <section className="address-section">
-      <div className="address--column">
-        <i className="fas fa-map-marker-alt"></i>
-      </div>
-      <div className="address">
-        <p>{generalDetails.address}</p>
-      </div>
-    </section>
-  );
-}
-
-function Name() {
-  const generalDetails = useSelector(selectGeneralDetails);
-  return (
-    <section className="name-section">
-      <h1 className="name">{`${generalDetails.firstName} ${generalDetails.lastName}`}</h1>
-      <p className="title">
-        <span>&#9871;</span> {generalDetails.title}
-      </p>
-    </section>
-  );
-}
-
-function Education() {
-  const education = useSelector(selectEducation);
-  const educationList = education.map((edu) => (
-    <div className="education-details" key={nanoid()}>
-      <h4 className="course-name">{edu.course}</h4>
-      <h4 className="institution-name">{edu.institution}</h4>
-      <p className="education-year">{edu.graduationYear}</p>
-    </div>
-  ));
-
-  return (
-    <section className="education-section">
-      <h2>EDUCATION</h2>
-      <hr></hr>
-      {educationList}
-    </section>
-  );
-}
-
-function Skills() {
-  const skills = useSelector(selectSkills);
-  const skillsList = skills.map((skill) => (
-    <li key={nanoid()}>{skill.text}</li>
-  ));
-  return (
-    <section className="skills-section">
-      <h2>SKILLS</h2>
-      <hr></hr>
-      <div className="skill-details">
-        <ul>{skillsList}</ul>
-      </div>
-    </section>
-  );
-}
-
-function Certifications() {
-  const certifications = useSelector(selectCertification);
-  const certificationList = certifications.map((certification) => (
-    <div className="certification-details" key={nanoid()}>
-      <h5 className="certification-name">{certification.name}</h5>
-      <p className="certification-year">{certification.achievedYear} {certification.expirationYear && "-"} {certification.expirationYear}</p>
-    </div>
-  ));
-  return (
-    <section className="certifications-section">
-      <h2>CERTIFICATIONS</h2>
-      <hr></hr>
-      {certificationList}
-    </section>
-  );
-}
-
-// RIGHT SECTION
-
-function Contact() {
-  const generalDetails = useSelector(selectGeneralDetails);
-
-  function generateInitials() {
-    let firstNameInitial = generalDetails.firstName.charAt(0);
-    let secondNameInitial = generalDetails.lastName.charAt(0);
-    return `${firstNameInitial}${secondNameInitial}`;
-  }
-
-  return (
-    <section className="contact-section">
-      <div className="initials">
-        <h4>{generateInitials()}</h4>
-      </div>
-      <div className="contact-details">
-        <p>
-          <i className="fas fa-envelope"></i>
-          {generalDetails.email}
-        </p>
-        <p>
-          <i className="fas fa-mobile"></i> {generalDetails.phone}
-        </p>
-        <p>
-          <i className="fas fa-globe"></i>
-          {generalDetails.website}
-        </p>
-      </div>
-    </section>
-  );
-}
-
-function Profile() {
-  const profile = useSelector(selectProfile);
-  return (
-    <section className="profile-section">
-      <h2>PROFILE</h2>
-      <hr></hr>
-      <p>{profile}</p>
-    </section>
-  );
-}
-
-function Experience() {
-  const experience = useSelector(selectExperience);
-  const experienceList = experience.map((exp) => (
-    <div className="experience-details" key={nanoid()}>
-      <h4 className="role">{exp.role}</h4>
-      <h4 className="company-name">{exp.company}</h4>
-      <p className="experience-date">
-        &nbsp; | <strong>{exp.fromDate}</strong> to{" "}
-        <strong>{exp.toDate}</strong>
-      </p>
-      <ul>
-        {exp.responsibilities.map((responsibility) => (
-          <li key={nanoid()}>{responsibility}</li>
-        ))}
-      </ul>
-    </div>
-  ));
-  return (
-    <section className="experience-section">
-      <h2>EXPERIENCE</h2>
-      <hr></hr>
-
-      {experienceList}
-    </section>
-  );
-}
+import { selectTemplate } from "../resumeSlice";
 
 export default function Preview() {
   const template = useSelector(selectTemplate);
+
   function FormNavigation() {
     return (
       <div className="formNavigation">
@@ -183,24 +27,33 @@ export default function Preview() {
     );
   }
 
+  const style = {
+    height: "3em",
+    fontSize: "0.8em",
+    padding: "1em 2em",
+    width: "11em",
+  };
+
   return (
     <div className="preview">
-      <div className="resume">
-        <div className="left">
-          <Address />
-          <Name name={"name"} title="Fullstack developer" />
-          <Education />
-          <Skills />
-          <Certifications />
-        </div>
-
-        <div className="right">
-          <Contact />
-          <Profile />
-          <Experience />
+      <div className="previewNavigation">
+        <p>Change template</p>
+        <div className="buttons">
+          {template !== "typeOne" && (
+            <button className="secondary-button">Template One</button>
+          )}
+          {template !== "typeTwo" && (
+            <button className="secondary-button">Template Two</button>
+          )}
+          {template !== "typeThree" && (
+            <button className="secondary-button">Template Three</button>
+          )}
         </div>
       </div>
-
+      {template === "typeOne" && <TypeOne />}
+      {template === "typeTwo" && <TypeTwo />}
+      {template === "typeThree" && <TypeThree />}
+      
       <FormNavigation />
     </div>
   );
---
+++ src/components/Templates/TypeThree.js
@@ -0,0 +1,206 @@
+import React from "react";
+import "../../styles/TypeOne.css";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  selectTemplate,
+  selectProfile,
+  selectEducation,
+  selectSkills,
+  selectExperience,
+  selectCertification,
+  selectGeneralDetails,
+} from "../../resumeSlice";
+import { nanoid } from "nanoid";
+import { Link } from "react-router-dom";
+
+// LEFT SECTION
+
+function Address() {
+  const generalDetails = useSelector(selectGeneralDetails);
+  return (
+    <section className="address-section">
+      <div className="address--column">
+        <i className="fas fa-map-marker-alt"></i>
+      </div>
+      <div className="address">
+        <p>{generalDetails.address}</p>
+      </div>
+    </section>
+  );
+}
+
+function Name() {
+  const generalDetails = useSelector(selectGeneralDetails);
+  return (
+    <section className="name-section">
+      <h1 className="name">{`${generalDetails.firstName} ${generalDetails.lastName}`}</h1>
+      <p className="title">
+        <span>&#9871;</span> {generalDetails.title}
+      </p>
+    </section>
+  );
+}
+
+function Education() {
+  const education = useSelector(selectEducation);
+  const educationList = education.map((edu) => (
+    <div className="education-details" key={nanoid()}>
+      <h4 className="course-name">{edu.course}</h4>
+      <h4 className="institution-name">{edu.institution}</h4>
+      <p className="education-year">{edu.graduationYear}</p>
+    </div>
+  ));
+
+  return (
+    <section className="education-section">
+      <h2>EDUCATION</h2>
+      <hr></hr>
+      {educationList}
+    </section>
+  );
+}
+
+function Skills() {
+  const skills = useSelector(selectSkills);
+  const skillsList = skills.map((skill) => (
+    <li key={nanoid()}>{skill.text}</li>
+  ));
+  return (
+    <section className="skills-section">
+      <h2>SKILLS</h2>
+      <hr></hr>
+      <div className="skill-details">
+        <ul>{skillsList}</ul>
+      </div>
+    </section>
+  );
+}
+
+function Certifications() {
+  const certifications = useSelector(selectCertification);
+  const certificationList = certifications.map((certification) => (
+    <div className="certification-details" key={nanoid()}>
+      <h5 className="certification-name">{certification.name}</h5>
+      <p className="certification-year">
+        {certification.achievedYear} {certification.expirationYear && "-"}{" "}
+        {certification.expirationYear}
+      </p>
+    </div>
+  ));
+  return (
+    <section className="certifications-section">
+      <h2>CERTIFICATIONS</h2>
+      <hr></hr>
+      {certificationList}
+    </section>
+  );
+}
+
+// RIGHT SECTION
+
+function Contact() {
+  const generalDetails = useSelector(selectGeneralDetails);
+
+  function generateInitials() {
+    let firstNameInitial = generalDetails.firstName.charAt(0);
+    let secondNameInitial = generalDetails.lastName.charAt(0);
+    return `${firstNameInitial}${secondNameInitial}`;
+  }
+
+  return (
+    <section className="contact-section">
+      <div className="initials">
+        <h4>{generateInitials()}</h4>
+      </div>
+      <div className="contact-details">
+        <p>
+          <i className="fas fa-envelope"></i>
+          {generalDetails.email}
+        </p>
+        <p>
+          <i className="fas fa-mobile"></i> {generalDetails.phone}
+        </p>
+        <p>
+          <i className="fas fa-globe"></i>
+          {generalDetails.website}
+        </p>
+      </div>
+    </section>
+  );
+}
+
+function Profile() {
+  const profile = useSelector(selectProfile);
+  return (
+    <section className="profile-section">
+      <h2>PROFILE</h2>
+      <hr></hr>
+      <p>{profile}</p>
+    </section>
+  );
+}
+
+function Experience() {
+  const experience = useSelector(selectExperience);
+  const experienceList = experience.map((exp) => (
+    <div className="experience-details" key={nanoid()}>
+      <h4 className="role">{exp.role}</h4>
+      <h4 className="company-name">{exp.company}</h4>
+      <p className="experience-date">
+        &nbsp; | <strong>{exp.fromDate}</strong> to{" "}
+        <strong>{exp.toDate}</strong>
+      </p>
+      <ul>
+        {exp.responsibilities.map((responsibility) => (
+          <li key={nanoid()}>{responsibility}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+  return (
+    <section className="experience-section">
+      <h2>EXPERIENCE</h2>
+      <hr></hr>
+
+      {experienceList}
+    </section>
+  );
+}
+
+export default function TypeThree() {
+  const template = useSelector(selectTemplate);
+  function FormNavigation() {
+    return (
+      <div className="formNavigation">
+        <Link
+          to="/resumeform/skills"
+          style={{
+            textDecoration: "none",
+          }}
+        >
+          <button>BACK</button>
+        </Link>
+
+        <button className="next">DOWNLOAD</button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="resume">
+      <div className="left">
+        <Address />
+        <Name name={"name"} title="Fullstack developer" />
+        <Education />
+        <Skills />
+        <Certifications />
+      </div>
+
+      <div className="right">
+        <Contact />
+        <Profile />
+        <Experience />
+      </div>
+    </div>
+  );
+}
